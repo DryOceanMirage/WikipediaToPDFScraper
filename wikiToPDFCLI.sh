8b08a01ce2309ce42fb88d0887d062b3f0e89fc7
DryOceanMirage <david.pasko.work@gmail.com>
2024-05-31 00:46:57 +1000

    main menu

--- wikiToPDFCLI.sh
+++ wikiToPDFCLI.sh
@@ -1,1 +1,12 @@
 source ./getListWiki.sh
+
+mainMenu(){
+clear
+echo "-------------------------------------"
+echo "Pick Choice"
+echo "1. Renew list"
+echo "2. Search list"
+echo "3. Download one"
+echo "4. Download all"
+echo "-------------------------------------"
+}
