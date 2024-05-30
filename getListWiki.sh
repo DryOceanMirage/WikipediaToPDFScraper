cbeb092563a55157909803c9df889dbc5bc3945c
DryOceanMirage <david.pasko.work@gmail.com>
2024-05-31 00:37:09 +1000

    grep if for next page

--- getListWiki.sh
+++ getListWiki.sh
@@ -1,6 +1,17 @@
 FIRST="https://en.wikipedia.org/wiki/Special:AllPages"
 PREVPAGE="$FIRST"
+getNextPage(){
 NEXTPAGE="https://en.wikipedia.org$(curl -s "$PREVPAGE" | grep "Next page" | tail -n 1 | grep -o '<a[^>]*href="[^"]*"[^>]*>Next page' | sed 's/^<a href="//' | sed 's/&amp;/\&/g' | sed 's/" title="Special:AllPages">Next page.*$//')"
+
+if curl -s "$NEXTPAGE" | grep -q "Next page"; then
+    echo "Getting Titles from page $pageNo"
+    curl "$NEXTPAGE" | grep -A 360 '<ul class="mw-allpages-chunk">' | sed -n 's/.*>\(.*\)<\/a>.*/\1/p' | grep -v 'Next page'| grep -v 'http' | grep -v 'Privacy policy' | grep -v 'About Wikipedia' | grep -v 'Disclaimers' | sed 's/&quot;/\"/g' | sed "s/&#039;/'/g" >> titles.txt && PREVPAGE="$NEXTPAGE" && getNextPage
+    
+else
+    curl "$NEXTPAGE" | grep -A 360 '<ul class="mw-allpages-chunk">' | sed -n 's/.*>\(.*\)<\/a>.*/\1/p' | grep -v 'Next page'| grep -v 'http' | grep -v 'Privacy policy' | grep -v 'About Wikipedia' | grep -v 'Disclaimers' | sed 's/&quot;/\"/g' | sed "s/&#039;/'/g" >> titles.txt
+    
+fi
+}
 PREVPAGE="$NEXTPAGE"
 
 echo "Getting Titles from page 1"
